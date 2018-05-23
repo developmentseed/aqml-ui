cce30c6b2321db8460c944bd92d03b518d8deae4
Anna Scalamogna <anna@developmentseed.org>
2018-05-23 15:58:14 -0400

    add map panel and style

--- app/assets/scripts/views/map.js
+++ app/assets/scripts/views/map.js
@@ -9,7 +9,32 @@ class MapView extends React.Component {
       <div className='map'>
         <div className='inner'>
           <Nav />
-          <Map />
+        </div>
+        <Map />
+        <div className='inner'>
+          <div className='map__panel'>
+            <div className='panel__header'>
+              <img className='time-icon' src='assets/graphics/content/moon.svg'/>
+              <h1 className='panel__title'>Houston</h1>
+              <h2 className='panel__subtitle'>Texas</h2>
+              <h3 className='panel__temperature'>82°</h3>
+              <p className='panel__date'>May 15, 2018</p>
+            </div>
+            <div className='panel__key'>
+              <p className='note panel__item'>Data Refreshed 15 min ago.</p>
+              <p className='label panel__item station-key'>Ground Station</p>
+              <p className='label'>Air Quality Index <a href=''>(USEPA)</a></p>
+              <p className='label'>Air Quality Opacity</p>
+            </div>
+            <div className='panel__footer'>
+              <p className='label'>WHO Fine Particulate Matter Guidelines:</p>
+              <ul className='guidelines-list'>
+                <li>10 μg/m3 annual mean</li>
+                <li>25 μg/m3 24-hour mean</li>
+              </ul>
+              <a href=''>More Information</a>
+            </div>
+          </div>
         </div>
       </div>
     );
