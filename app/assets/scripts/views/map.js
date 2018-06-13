1ee609e0434613a48679d48dc54181e6097acbbe
Anna Scalamogna <anna@developmentseed.org>
2018-06-13 11:53:55 -0400

    adjust colors, add averages

--- app/assets/scripts/views/map.js
+++ app/assets/scripts/views/map.js
@@ -24,7 +24,14 @@ class MapView extends React.Component {
               <p className='note panel__item'>Data Refreshed 15 min ago.</p>
               <p className='label panel__item station-key'>Ground Station</p>
               <p className='label'>Air Quality Index <a href=''>(USEPA)</a></p>
-              <p className='label'>Air Quality Opacity</p>
+              <ul className='index-key-list'>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
             </div>
             <div className='panel__footer'>
               <p className='label'>WHO Fine Particulate Matter Guidelines:</p>
@@ -33,6 +40,18 @@ class MapView extends React.Component {
                 <li>25 μg/m3 24-hour mean</li>
               </ul>
               <a href=''>More Information</a>
+            </div>
+          </div>
+          <div className='map__panel aggregates'>
+            <div className='panel__header'>
+              <h1 className='label'>City Averages Today</h1>
+              <ul className='reading-list'>
+                <li>112<span>μg/m³</span><small>model estimate</small></li>
+                <li>112<span>μg/m³</span><small>ground stations</small></li>
+              </ul>
+            </div>
+            <div className='panel__body'>
+              <h2 className='label'>Readings Over the Last 24 Hours</h2>
             </div>
           </div>
         </div>
